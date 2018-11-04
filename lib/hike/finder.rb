de089171a779f5d1ac5c8a68e9fe35ee3cb695fe
Stef Claus <stef.claus@gmail.com>
2018-11-04 20:39:00 +0000

    first commit

---
+++ lib/hike/finder.rb
@@ -0,0 +1,7 @@
+require "hike/finder/version"
+
+module Hike
+  module Finder
+    # Your code goes here...
+  end
+end
---
+++ lib/hike/finder/version.rb
@@ -0,0 +1,5 @@
+module Hike
+  module Finder
+    VERSION = "0.1.0"
+  end
+end
