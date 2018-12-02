903bcae4865c3d09063b3f4afe166a82bb1b6096
Stef Claus <stef.claus@gmail.com>
2018-12-02 20:47:23 +0000

    new enviroment file

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/hike/scraper'
+require_relative '../lib/hike/finder'
+require_relative '../lib/hike/cli'
+require_relative '../lib/hike/hikes'
