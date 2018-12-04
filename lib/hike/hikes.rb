6b2062e1182f39b6f752c0b897d38638888f8adb
Stef Claus <stef.claus@gmail.com>
2018-12-04 03:01:36 +0000

    changes to scaper class

--- lib/hike/hikes.rb
+++ lib/hike/hikes.rb
@@ -4,14 +4,14 @@ class HikeFinder::Hikes
 
   @@all = []
 
- #  def self.new_from_index_page(r)
-#    self.new(
-#      r.css("h2").text,
-#      "https://www.theworlds50best.com#{r.css("a").attribute("href").text}",
-#      r.css("h3").text,
-#      r.css(".position").text
-#      )
-#  end
+   def self.new_from_index_page(r)
+    self.new(
+    #  r.css("h2").text,
+    #  "https://www.theworlds50best.com#{r.css("a").attribute("href").text}",
+    #  r.css("h3").text,
+     # r.css(".position").text
+     )
+  end
 
   def initialize(name=nil, length=nil, difficulty=nil, streams=nil, views=nil, solitude=nil, camping=nil)
     @name = name
--- lib/hike/scraper.rb
+++ lib/hike/scraper.rb
@@ -1,22 +1,19 @@
 class  HikeFinder::Scraper
 
-#don't know if I should use this one or the one below 
- # def get_page
-#   Nokogiri::HTML(open("https://www.hikingupward.com/maps/"))
-#  end
-  
-  
-  def self.scrape_hikes_index 
-    doc = Nokogiri::HTML(open("https://www.hikingupward.com/maps/"))
-    binding.pry 
+def get_page
+    Nokogiri::HTML(open("https://www.hikingupward.com/maps/"))
+    binding.pry
+  end
+
+  def scrape_hikes_index
+     self.get_page.css("ENTER FUNCTIONAL SCRAPE HERE")
   end
 
 
-
- # def make_hikes
-  #  scrape_hikes_index.each do |r|
-#      HikeFinder::Hikes.new_from_index_page(r)
-#  end
-#end 
+  def make_hikes
+    scrape_hikes_index.each do |r|
+      HikeFinder::Hikes.new_from_index_page(r)
+    end
+  end 
 
 end
