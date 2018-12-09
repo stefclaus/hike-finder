a531c5ad053bcde7adbf4df89c0bc4a7fc96413d
Stef Claus <stef.claus@gmail.com>
2018-12-09 19:04:12 +0000

    SCraper method tweaks

--- lib/hike/scraper.rb
+++ lib/hike/scraper.rb
@@ -1,31 +1,23 @@
 class  HikeFinder::Scraper
 
-  def self.get_page
-    session = Capybara::Session.new(:poltergeist)
-    session.visit('https://www.hikingupward.com/maps/')
-    element = session.first('.hike') #this gives us Beartown State Hike and Hike Length 
-    element #this gives us Beartown State Hike and Hike Length 
-  end
+#  def self.get_page
+  #  session = Capybara::Session.new(:poltergeist)
+   # session.visit('https://www.hikingupward.com/maps/')
+  #  element = session.first('.hike') #this gives us Beartown State Hike and Hike Length 
+   # element.text.strip #this gives us Beartown State Hike and Hike Length 
+#  end
 
 
   def self.make_hikes
     session = Capybara::Session.new(:poltergeist)
     session.visit('https://www.hikingupward.com/maps/')
-    element = session.all('.hike')  
+    element = session.all('.hike')
     element.each do |hike|
       HikeFinder::Hikes.new_from_index_page(hike)
+      binding.pry
     end
   end 
   
-  
-  def self.scrape_hikes 
-    session = Capybara::Session.new(:poltergeist)
-    index_page = session.visit('https://www.hikingupward.com/maps/')
-    
-    array_of_hikes = index_page.all(".hike")
-    binding.pry 
-  end 
-
   
 
 end
