eaf0c3167e1390cd9e310bffe28fb7cfa0e13dd2
Stef Claus <stef.claus@gmail.com>
2018-12-16 23:11:12 +0000

    Tweaks to cli method

--- lib/hike/cli.rb
+++ lib/hike/cli.rb
@@ -21,7 +21,7 @@ class HikeFinder::CLI
       puts "How long do you want your mini-hike to be?"
       puts "Type 1 for 1 mile hikes, 2 for 2 mile hikes, or 3 for 3 mile hikes."
       length = gets.strip.to_i
-      experimenting(length)
+      print_hike(length)
     end
     
     def list_hikes
@@ -45,16 +45,8 @@ class HikeFinder::CLI
       puts ""
     end
 
-    def print_hike(length)
-      puts ""
-      puts "---------- #{length} Mile Hikes in the Mid-Atlantic Region ----------"
-      puts ""
-        HikeFinder::Hikes.all[length].each_index.select(length) do |hikes, index|
-          puts "#{index}. #{hikes.hike_name} - #{hikes.hike_length}"
-        end #ends iteration
-    end #ends class 
   
-   def experimenting(length)
+   def print_hike(length)
      puts "---------- #{length} Mile Hikes in the Mid-Atlantic Region ----------"
      if length == 1
         HikeFinder::Hikes.all[6, 19].each.with_index do |hike, index| 
